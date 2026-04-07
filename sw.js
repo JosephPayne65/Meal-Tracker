7a527e594fd83e0255d8f334cce1398c621fc09a
Joseph Payne <joepayne622@gmail.com>
2026-04-07 15:01:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "macro-tracker-v18";
+const CACHE_NAME = "macro-tracker-v19";
 
 // On install, skip waiting immediately
 self.addEventListener("install", () => self.skipWaiting());
